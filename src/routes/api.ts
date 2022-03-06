9402de3c97dea5fba9a18c54be670c33966eb3c3
GregCKrause <gregckrause@gmail.com>
2022-03-06 12:03:35 -0600

    Adding csrf-test API endpoint

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -22,4 +22,32 @@ apiRoutes.get(
   }
 )
 
+apiRoutes.get(
+  '/csrf-test',
+  (req: DefaultAPIRequest, res: express.Response): express.Response => {
+    try {
+      console.log('req.headers', JSON.stringify(req.headers))
+      console.log('req.cookies', JSON.stringify(req.cookies))
+      console.log('req.body', JSON.stringify(req.body))
+      return res.status(200).send('OK')
+    } catch (err) {
+      return res.status(500).send('An error occurred')
+    }
+  }
+)
+
+apiRoutes.post(
+  '/csrf-test',
+  (req: DefaultAPIRequest, res: express.Response): express.Response => {
+    try {
+      console.log('req.headers', JSON.stringify(req.headers))
+      console.log('req.cookies', JSON.stringify(req.cookies))
+      console.log('req.body', JSON.stringify(req.body))
+      return res.status(200).send('OK')
+    } catch (err) {
+      return res.status(500).send('An error occurred')
+    }
+  }
+)
+
 export { apiRoutes }
