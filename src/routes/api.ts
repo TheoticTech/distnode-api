b6371bf479de8473d0d6986e8fce8761fe973ecb
GregCKrause <gregckrause@gmail.com>
2022-03-23 23:43:13 -0500

    Adding server-side HTML sanitization

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,5 +1,6 @@
 // Third party
 import express from 'express'
+import sanitizeHtml from 'sanitize-html'
 
 // Local
 import { authMiddleware } from '../middleware/auth'
@@ -54,21 +55,62 @@ apiRoutes.post(
   ): Promise<express.Response> => {
     try {
       const { userID } = req
-      const { title, body, visibility } = req.body
+      const { title, body } = req.body
 
-      if (!title || !body || !visibility) {
+      if (!title) {
         return res.status(400).json({
-          addPostError: 'Missing title, body or visibility'
+          addPostError: 'Post title must not be empty'
         })
       }
+
+      if (!body) {
+        return res.status(400).json({
+          addPostError: 'Post body must not be empty'
+        })
+      }
+
+      const sanitizedBody = sanitizeHtml(body, {
+        allowedTags: sanitizeHtml.defaults.allowedTags.concat([
+          'img',
+          'iframe',
+          'video',
+          'audio',
+          'source'
+        ]),
+        allowedAttributes: {
+          img: [ 'src', 'width', 'height' ],
+          iframe: [ 'src', 'width', 'height', 'frameborder', 'allowfullscreen' ],
+          video: [ 'controls', 'width', 'height' ],
+          audio: [ 'controls', 'src' ],
+          source: [ 'src', 'type' ],
+          span: [ 'style' ],
+          p: [ 'style' ]
+        },
+        allowedIframeHostnames: ['www.youtube.com'],
+        disallowedTagsMode: 'escape',
+        selfClosing: [ 'img', 'source' ],
+        allowedSchemesByTag: {
+          img: [ 'https' ]
+        },
+        allowedStyles: {
+          '*': {
+            'text-align': [/^left$/, /^right$/, /^center$/, /^justify$/],
+            'text-decoration': [/^underline$/],
+            'padding-left': [/[0-9]{1,4}px$/]
+          },
+          'span': {
+            'background-color': [/^rgb\([0-9]{0,3}, [0-9]{0,3}, [0-9]{0,3}\)$/]
+          }
+        }
+      })
 
       await queryNeo4j(
         req.app.locals.driver,
         'MATCH (u:User {userID: $userID}) ' +
-          'CREATE (p:Post {title: $title, body: $body, visibility: $visibility})' +
+          'CREATE (p:Post {title: $title, body: $sanitizedBody})' +
           '-[:POSTED_BY]->(u) ' +
           'RETURN p',
-        { userID, title, body, visibility }
+        { userID, title, sanitizedBody }
       )
       return res.status(200).json({
         addPostSuccess: 'Post created successfully'
