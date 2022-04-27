3b98f8b7132796da06be17fb4000573d8ed35716
GregCKrause <gregckrause@gmail.com>
2022-04-27 18:32:54 -0500

    Showing existing reactions on Post and Profile view pages

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -192,19 +192,23 @@ apiRoutes.get(
 // Obtain posts created by same author as post with provided ID
 apiRoutes.get(
   '/post/:postID/related/author',
+  optionalAuthMiddleware,
   async (
     req: DefaultAPIRequest,
     res: express.Response
   ): Promise<express.Response> => {
     try {
+      const { userID = 'DNE' } = req
       const { postID } = req.params
 
       const results = await queryNeo4j(
         req.app.locals.driver,
         'MATCH (p:Post)-[:POSTED_BY]->(u:User) WHERE id(p) = $postID ' +
           'MATCH (po: Post)-[:POSTED_BY]->(u:User) WHERE id(po) <> id(p) ' +
-          'return po, u LIMIT 6',
-        { postID: parseInt(postID) }
+          'OPTIONAL MATCH (activeUser:User {userID: $userID}) ' +
+          'OPTIONAL MATCH (activeUser)-[:ReactionFrom]->(r:Reaction)-[:ReactionTo]->(po) ' +
+          'return po, u, r LIMIT 6',
+        { postID: parseInt(postID), userID }
       )
 
       return res.status(200).json({
@@ -212,6 +216,10 @@ apiRoutes.get(
         posts: results.records.map((r) => {
           const nodePostVal = r.get('po')
           const nodeUserVal = r.get('u')
+          const nodeReactionVal = r.get('r')
+          const reaction = nodeReactionVal
+          ? nodeReactionVal.properties.type
+          : null
           return {
             userID: nodeUserVal.properties.userID,
             username: nodeUserVal.properties.username,
@@ -220,7 +228,8 @@ apiRoutes.get(
             postCreatedAt: nodePostVal.properties.created_at.toNumber(),
             description: nodePostVal.properties.description,
             title: nodePostVal.properties.title,
-            thumbnail: nodePostVal.properties.thumbnail
+            thumbnail: nodePostVal.properties.thumbnail,
+            ...(reaction && { reaction })
           }
         })
       })
@@ -792,7 +801,6 @@ apiRoutes.get(
         }
       })
     } catch (err) {
-      console.log(err)
       return res.status(500).json({
         getUserError: 'An unknown error occurred, please try again later'
       })
@@ -803,19 +811,23 @@ apiRoutes.get(
 // Obtain supplied user's info and posts
 apiRoutes.get(
   '/user/:userID/profile',
+  optionalAuthMiddleware,
   async (
     req: DefaultAPIRequest,
     res: express.Response
   ): Promise<express.Response> => {
     try {
-      const userID = req.params.userID
+      const { userID = 'DNE' } = req
+      const userIDParam = req.params.userID
 
       const results = await queryNeo4j(
         req.app.locals.driver,
-        'MATCH (u:User {userID: $userID}) ' +
-          'OPTIONAL MATCH (p:Post)-[r:POSTED_BY]->(u) ' +
-          'return p, u ORDER BY p.created_at DESC',
-        { userID }
+        'MATCH (u:User {userID: $userIDParam}) ' +
+          'OPTIONAL MATCH (p:Post)-[pb:POSTED_BY]->(u) ' +
+          'OPTIONAL MATCH (activeUser:User {userID: $userID}) ' +
+          'OPTIONAL MATCH (activeUser)-[:ReactionFrom]->(r:Reaction)-[:ReactionTo]->(p) ' +
+          'return p, u, r ORDER BY p.created_at DESC',
+        { userIDParam, userID }
       )
 
       if (results.records.length === 0) {
@@ -841,6 +853,10 @@ apiRoutes.get(
           .filter((r) => r.get('p'))
           .map((r) => {
             const nodePostVal = r.get('p')
+            const nodeReactionVal = r.get('r')
+            const reaction = nodeReactionVal
+            ? nodeReactionVal.properties.type
+            : null
             // We'll keep user info in the post object for PostFeed
             return {
               userID: user.userID,
@@ -851,12 +867,12 @@ apiRoutes.get(
               postCreatedAt: nodePostVal.properties.created_at.toNumber(),
               description: nodePostVal.properties.description,
               title: nodePostVal.properties.title,
-              thumbnail: nodePostVal.properties.thumbnail
+              thumbnail: nodePostVal.properties.thumbnail,
+              ...(reaction && { reaction })
             }
           })
       })
     } catch (err) {
-      console.log(err)
       return res.status(500).json({
         getUserProfileError: 'An unknown error occurred, please try again later'
       })
